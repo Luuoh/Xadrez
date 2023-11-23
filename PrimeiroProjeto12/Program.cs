4aaa8c4aa0bd4315b584c3207531e9475cffd592
Luuoh <luizd.teo@outlook.com>
2023-11-23 18:27:02 -0300

    Melhorado a implementação do metodo Imprimir tabuleiro

--- PrimeiroProjeto12/Program.cs
+++ PrimeiroProjeto12/Program.cs
@@ -6,9 +6,22 @@ namespace PrimeiroProjeto12 {
     internal class Program {
         static void Main(string[] args) {
 
-            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+            try {
+                Tabuleiros tabuleiro = new Tabuleiros(8, 8);
+
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(2, 4));
+
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(7, 7));
+                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(6, 3));
+                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Branca), new Posicao(4, 5));
+
+                Tela.imprimirTabuleiro(tabuleiro);
+            }
+            catch (Exception e) { 
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
