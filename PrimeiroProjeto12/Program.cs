ee0f21a2074986721689c6d46db5a052a4b1bf14
Luuoh <luizd.teo@outlook.com>
2023-11-23 16:30:40 -0300

    Criando a classe posição

--- PrimeiroProjeto12/Program.cs
+++ PrimeiroProjeto12/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Xml;
+using Tabuleiro;
 
 namespace PrimeiroProjeto12 {
     internal class Program {
         static void Main(string[] args) {
-            
+            Posicao p = new Posicao(4, 3);
+            Console.WriteLine("Posição " + p);
         }
     }
 }
